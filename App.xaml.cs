038eb29601e67be9e15cb59fe094748c64315a57
sonia <eha80877@educastur.es>
2025-05-14 19:53:57 +0200

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace proyectoFin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
